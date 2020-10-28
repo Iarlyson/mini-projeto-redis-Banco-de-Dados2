f8464c15f9b801b0c195fecff9f972c10582aa3f
Iarlyson Santana <Iarlyson.Santana@gmail.com>
2020-10-28 11:00:18 -0300

    Correçao na busca de produto

--- api/src/controllers/product.controller.js
+++ api/src/controllers/product.controller.js
@@ -30,7 +30,7 @@ exports.listarTodosProdutos = async (req, res) => {
 
 exports.buscaProdutoPorId = async (req, res) => {
   const id = parseInt(req.params.id);
-  const response = await db.query('SELECT id FROM produto WHERE id = $1', [id]);
+  const response = await db.query('SELECT * FROM produto WHERE id = $1', [id]);
   res.status(200).send(response.rows);
   console.log(response.rows[0].id)
 }
